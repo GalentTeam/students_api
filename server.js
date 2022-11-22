e2b19f23eca8d2f4528d38db6864895f9ea60f3e
Galenteryan <galenteryan@gmail.com>
2022-11-23 00:12:01 +0400

    alphabet

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 // Limit requests from same IP
 app.use('/', rateLimit({
-    max: 200,
+    max: 300,
     windowMs: 60 * 60 * 1000,
     message: 'Too many requests from this IP, please try again in an hour!'
 }));
